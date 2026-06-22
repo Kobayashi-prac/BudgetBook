c6949cde527943e7070c70c1a22ba2b536487d9a
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2026-06-22 20:34:49 +0900

    First Commit
    :wq

---
+++ BudgetBook/BudgetBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBookApp.swift
+//  BudgetBook
+//
+//  Created by swiftUI on 2026/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetBookTests/BudgetBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBookTests.swift
+//  BudgetBookTests
+//
+//  Created by swiftUI on 2026/06/22.
+//
+
+import Testing
+@testable import BudgetBook
+
+struct BudgetBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

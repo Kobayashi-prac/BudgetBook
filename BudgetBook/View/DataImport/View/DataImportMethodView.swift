7dbcb7b58fc4e1e877bb118003c733705dcc9e1e
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2026-07-06 00:06:02 +0900

    追加手段をデザイン通りに実装

--- BudgetBook/View/DataImport/View/DataImportMethodView.swift
+++ BudgetBook/View/DataImport/View/DataImportMethodView.swift
@@ -9,31 +9,46 @@ import SwiftUI
 
 struct DataImportMethodView: View {
     
-    @Environment(\.rootSize) var rootSize: CGSize
-    
     var body: some View {
-        Button {
-            
-        } label: {
-            HStack {
-                Image(systemName: "keyboard")
-                    .resizable()
-                    .frame(width: 20, height: 40)
-                    .background(Color.black)
-                VStack {
-                    Text("手入力")
-                    Text("\(rootSize.width)")
+        VStack(alignment: .leading) {
+            Button {
+                
+            } label: {
+                HStack(alignment: .center, spacing: 0) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12)
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(Color(.systemGray5))
+                            .padding(.horizontal, 20)
+                        Image(systemName: "keyboard")
+                            .resizable()
+                            .foregroundStyle(.black)
+                            .frame(width: 25, height: 20)
+                    }
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("手入力")
+                            .font(.system(size: 20, weight: .semibold))
+                            .foregroundStyle(.black)
+                        Text("金額・カテゴリを直接入力")
+                            .foregroundStyle(.gray)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                    Image(systemName: "chevron.right")
+                        .foregroundStyle(.gray)
+                        .padding(.trailing, 20)
                 }
-                .foregroundStyle(Color.black)
+            }
+            .frame(maxWidth: .infinity, maxHeight: 100)
+            .overlay {
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(.secondary, lineWidth: 2)
             }
         }
-        .frame(width: rootSize.width, height: 100)
+        // 内部の要素に余白を設ける
+        .padding()
     }
 }
 
 #Preview {
-    GeometryReader { geo in
-        DataImportMethodView()
-            .environment(\.rootSize, geo.size)
-    }
+    DataImportMethodView()
 }

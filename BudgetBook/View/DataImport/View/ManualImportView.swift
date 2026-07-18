9eda03ede7fc291b231d0424f998216c1a71756b
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2026-07-19 02:25:51 +0900

    TextFieldを追加

--- BudgetBook/View/DataImport/View/ManualImportView.swift
+++ BudgetBook/View/DataImport/View/ManualImportView.swift
@@ -14,46 +14,30 @@ enum TransactionType: String, CaseIterable {
 
 struct ManualImportView: View {
     
-    @Namespace private var select
-    
-    @State private var selection: Int = 0
-    
-    let transactions = TransactionType.allCases
+    @State private var amount: Int?
+    @FocusState private var isFocused: Bool
     
     var body: some View {
         VStack {
-            Group {
-                HStack(spacing: 0) {
-                    ForEach(transactions.indices, id: \.self) { index in
-                        Text(transactions[index].rawValue)
-                            .font(.subheadline.bold())
-                            .foregroundStyle(selection == index ? .white : .secondary)
-                            .frame(maxWidth: .infinity)
-                            // 高さかpaddingか
-                            .padding(.vertical, 10)
-                            .background {
-                                if selection == index {
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .fill(.red)
-                                        .matchedGeometryEffect(id: "selectType",
-                                                               in: select)
-                                }
-                            }
-                            .contentShape(.rect)
-                            .onTapGesture {
-                                withAnimation(.easeInOut(duration: 0.3)) {
-                                    selection = index
-                                }
-                            }
-                    }
+            TransactionTypeTab()
+                .padding()
+            
+            Text("金額")
+                .opacity(0.5)
+            
+            TextField("0", value: $amount, format: .number)
+                .font(.system(size: 50, weight: .bold, design: .rounded))
+                .fixedSize()
+                .multilineTextAlignment(.center)
+                .focused($isFocused)
+                .keyboardType(.numberPad)
+                .submitLabel(.done)
+                .onSubmit {
+                    isFocused = false
                 }
-                .padding(4)
-                .background(
-                    RoundedRectangle(cornerRadius: 12)
-                        .fill(Color(.systemGray5))
-                )
-            }
-            .padding()
+                .onAppear {
+                    isFocused = true
+                }
         }
         .navigationTitle("取引を追加")
         .toolbar {
@@ -61,6 +45,47 @@ struct ManualImportView: View {
                 Text("保存")
             }
         }
+    }
+}
+
+struct TransactionTypeTab: View {
+    
+    @Namespace private var select
+    
+    @State private var selection: Int = 0
+    
+    let transactions = TransactionType.allCases
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(transactions.indices, id: \.self) { index in
+                Text(transactions[index].rawValue)
+                    .font(.subheadline.bold())
+                    .foregroundStyle(selection == index ? .white : .secondary)
+                    .frame(maxWidth: .infinity)
+                     // 高さかpaddingか
+                    .padding(.vertical, 10)
+                    .background {
+                        if selection == index {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(.red)
+                                .matchedGeometryEffect(id: "selectType",
+                                                       in: select)
+                        }
+                    }
+                    .contentShape(.rect)
+                    .onTapGesture {
+                        withAnimation(.easeInOut(duration: 0.3)) {
+                            selection = index
+                        }
+                    }
+            }
+        }
+        .padding(4)
+        .background(
+            RoundedRectangle(cornerRadius: 12)
+                .fill(Color(.systemGray5))
+        )
     }
 }
 

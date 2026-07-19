99612a7b95aed9e9c3d4ff7d2b54aac99e7fb6ae
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2026-07-19 14:43:07 +0900

    カテゴリビューを追加

--- BudgetBook/View/DataImport/View/ManualImportView.swift
+++ BudgetBook/View/DataImport/View/ManualImportView.swift
@@ -12,8 +12,70 @@ enum TransactionType: String, CaseIterable {
     case income = "収入"
 }
 
-enum TransactionCategory {
+enum TransactionCategory: CaseIterable, Identifiable {
     
+    var id: Self {
+        self
+    }
+    
+    /// 食費
+    case food
+    /// 日用品
+    case dailyGoods
+    /// 交通
+    case transport
+    /// 住居
+    case housing
+    /// 娯楽
+    case entertainment
+    /// 医療
+    case medical
+    /// カフェ
+    case cafe
+    /// その他
+    case other
+    
+    var name: String {
+        switch self {
+        case .food:
+            return "食費"
+        case .dailyGoods:
+            return "日用品"
+        case .transport:
+            return "交通"
+        case .housing:
+            return "住居"
+        case .entertainment:
+            return "娯楽"
+        case .medical:
+            return "医療"
+        case .cafe:
+            return "カフェ"
+        case .other:
+            return "その他"
+        }
+    }
+    
+    var iconName: String {
+        switch self {
+        case .food:
+            return "fork.knife"
+        case .dailyGoods:
+            return "bag.fill"
+        case .transport:
+            return "tram.fill"
+        case .housing:
+            return "house"
+        case .entertainment:
+            return "gamecontroller"
+        case .medical:
+            return "heart.text.square"
+        case .cafe:
+            return "cup.and.saucer"
+        case .other:
+            return "ellipsis"
+        }
+    }
 }
 
 struct ManualImportView: View {
@@ -30,6 +92,9 @@ struct ManualImportView: View {
             Text("金額")
                 .opacity(0.5)
             
+            // TODO: .focused($isFocused)で下にズレる
+            // TODO: 横幅の制限
+            // TODO: 円の表示
             TextField("0", value: $amount, format: .number)
                 .font(.system(size: 50, weight: .bold, design: .rounded))
                 .fixedSize()
@@ -40,12 +105,7 @@ struct ManualImportView: View {
                     isFocused = true
                 }
             
-            VStack {
-                Text("カテゴリ")
-                    .padding(.horizontal, 16)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-            }
+            CategoryView()
             
             Spacer()
         }
@@ -85,7 +145,7 @@ struct TransactionTypeTab: View {
                     .font(.subheadline.bold())
                     .foregroundStyle(selection == index ? .white : .secondary)
                     .frame(maxWidth: .infinity)
-                     // 高さかpaddingか
+                     // TODO: 高さかpaddingか
                     .padding(.vertical, 10)
                     .background {
                         if selection == index {
@@ -108,6 +168,52 @@ struct TransactionTypeTab: View {
             RoundedRectangle(cornerRadius: 12)
                 .fill(Color(.systemGray5))
         )
+    }
+}
+
+struct CategoryView: View {
+    
+    @State private var isSelectedCategory: TransactionCategory?
+    
+    var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 4)
+    
+    var body: some View {
+        VStack {
+            Text("カテゴリ")
+                .padding(.horizontal, 16)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            LazyVGrid(columns: columns) {
+                ForEach(TransactionCategory.allCases) { category in
+                    VStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 12)
+                                .frame(width: 55, height: 55)
+                                .foregroundStyle(Color(.systemGray5))
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 12)
+                                    .strokeBorder(
+                                        isSelectedCategory == category ? .black : .clear,
+                                        lineWidth: 2
+                                    )
+                                )
+                            Image(systemName: category.iconName)
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundStyle(.black)
+                                .frame(width: 22, height: 22)
+                        }
+                        .onTapGesture {
+                            isSelectedCategory = category
+                        }
+                        Text("\(category.name)")
+                            .opacity(isSelectedCategory == category ? 1 : 0.5)
+                            .font(.system(size: 15))
+                    }
+                }
+            }
+            .padding()
+        }
     }
 }
 
